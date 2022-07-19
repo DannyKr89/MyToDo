63ac5f2344108c71f65700ea0814a7b92d939e9b
dk <omnipresent9@yandex.ru>
2022-07-19 20:21:15 +0300

    ДЗ 7

--- app/src/main/java/com/example/mytodo/MainActivity.java
+++ app/src/main/java/com/example/mytodo/MainActivity.java
@@ -1,16 +1,13 @@
 package com.example.mytodo;
 
-import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatTextView;
-import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -19,9 +16,9 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Notes> arrNotes;
+    private final String[] colors = {"#AAAAAA", "#CCCCCC"};
     Date date = Calendar.getInstance().getTime();
 
-    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,62 +31,38 @@ public class MainActivity extends AppCompatActivity {
         arrNotes.add(new Notes("Приготовить еду", "рашгфрdsafasdfыагфыа", date));
         arrNotes.add(new Notes("Приготовить еду", "рашгфрыаasdfsaгфыа", date));
         arrNotes.add(new Notes("Приготовить еду", "рашгфрыаadsfasdfгфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготasdasdasdasfsadsggdgsdffggdfgfsdовить еду", "рашгфрыагфыа", date));
-        arrNotes.add(new Notes("Приготовить еду", "рашгфрыагфыа", date));
+        arrNotes.add(new Notes("Приготовить еду sfsdafadfaf", "рашгфрыагфыа", date));
 
-        updateNotes();
-
+        initNotes();
 
     }
 
-    public void updateNotes() {
-        LinearLayout layout = findViewById(R.id.frameLL);
-        String[] colors = {"#CCCCCC","#FFFFFF"};
-
+    public void initNotes() {
+        LinearLayout layout = findViewById(R.id.frameNotes)  ;
         for (int i = 0; i < arrNotes.size(); i++) {
-            AppCompatTextView tv = new AppCompatTextView(MainActivity.this);
+            TextView tv = new TextView(MainActivity.this);
             tv.setText(arrNotes.get(i).getTitle());
             tv.setTextSize(30);
-            tv.setPadding(10,10,10,0);
+            tv.setPadding(10, 10, 10, 10);
             tv.setSingleLine(true);
             tv.setBackgroundColor(Color.parseColor(colors[i % 2]));
-
             layout.addView(tv);
-            int index = i;
-            tv.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Bundle bundle = new Bundle();
-                    bundle.putParcelable("note", arrNotes.get(index));
-                    NotesFragment nf = new NotesFragment();
-                    nf.setArguments(bundle);
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                            .add(R.id.frame, nf)
-                            .addToBackStack("")
-                            .commit();
-                }
+            final int index = i;
+            tv.setOnClickListener(v -> {
+                showNote(arrNotes.get(index));
             });
         }
     }
+
+    private void showNote(Notes notes) {
+        NoteFragment nf = NoteFragment.newInstance(notes);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                .replace(R.id.frameLL, nf)
+                .addToBackStack("")
+                .commit();
+    }
+
+
 }
